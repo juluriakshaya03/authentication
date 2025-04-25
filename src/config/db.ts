9df7a8154ae3c29ad4dd6807c113505ace970682
Mykola Serdiuk <mykola_serdiuk@epam.com>
2025-04-25 14:45:51 +0300

    Fix db connect and update gitignore

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,27 +1,18 @@
-jest.mock("../../controllers/user.controller"); // от корня до mock-файла
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import express from "express";
-import request from "supertest";
-import userRoutes from "../../routes/user.routes";
+dotenv.config();
 
-const app = express();
-app.use(express.json());
-app.use("/", userRoutes);
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/test";
 
-describe("User Routes", () => {
-  it("GET / should return mocked user list", async () => {
-    const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body[0].name).toBe("Mock User");
-  });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
 
-  it("POST /add should return created user", async () => {
-    const res = await request(app)
-      .post("/add")
-      .send({ name: "Alice", email: "alice@example.com" });
-
-    expect(res.statusCode).toBe(201);
-    expect(res.body.name).toBe("Alice");
-  });
-});
+export default connectDB;
